9ee3e6a092e9aadd865ac0ff2e086724943b311a
levancaothe <thel2230@gmail.com>
2026-05-25 18:34:36 +0700

    Add environment variable  for Fe

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  // baseURL: 'http://localhost:5000/api',
+
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
