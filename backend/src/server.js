b963008e79bb9877bd078a5db521e030861a9cb3
levancaothe <thel2230@gmail.com>
2026-05-26 17:47:19 +0700

    Add AI chatbox feature

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,9 +4,8 @@ const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 5001;
 
-// Connect to Database
 connectDB();
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
