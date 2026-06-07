ba54d78c032cc3634858651772b8f3c2c5e80eec
nhsonn1 <sonnhhe171238@fpt.edu.vn>
2026-06-07 20:02:49 +0700

    Added Collection Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import ErrorBoundary from './components/ErrorBoundary';
-import Home from './pages/Home';
-import CharmListPage from './pages/CharmListPage';
-import DesignerPage from './pages/DesignerPage';
-import MyDesignsPage from './pages/MyDesignsPage';
-import CartPage from './pages/CartPage';
-import OrdersPage from './pages/OrdersPage';
-import AdminDashboard from './pages/AdminDashboard';
-import GuidePage from './pages/GuidePage';
-import AboutUsPage from './pages/AboutUsPage';
-import { CartProvider } from './context/CartContext';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import ErrorBoundary from "./components/ErrorBoundary";
+import Home from "./pages/Home";
+import CharmListPage from "./pages/CharmListPage";
+import DesignerPage from "./pages/DesignerPage";
+import MyDesignsPage from "./pages/MyDesignsPage";
+import CartPage from "./pages/CartPage";
+import OrdersPage from "./pages/OrdersPage";
+import AdminDashboard from "./pages/AdminDashboard";
+import GuidePage from "./pages/GuidePage";
+import AboutUsPage from "./pages/AboutUsPage";
+import { CartProvider } from "./context/CartContext";
+import CollectionsPage from "./pages/CollectionsPage";
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
               <Route path="/about-us" element={<AboutUsPage />} />
               <Route path="/policy" element={<GuidePage />} />
               <Route path="/about" element={<GuidePage />} />
+              <Route path="/collections" element={<CollectionsPage />} />
             </Routes>
           </Layout>
         </Router>
